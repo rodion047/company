8d7a6a2732871b4c0bef104541a1858da7b06321
Rodion M <rodion047@gmail.com>
2012-09-14 17:14:10 -0400

    Team 1 added lines to main(), commented out 1 line

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@ int main() {
 	companyFeature_2();
 	companyFeature_3();
 
-	companyFeature_4(); // added by a 'main user'
+	// Commented out by the Team 1
+	// companyFeature_4(); // added by a 'main user'
 	anotherUserCall_1();
+
+	// team 1 code
+	int a;
+	while(1) {
+		// do
+	}
+	// end of team 1 code
 }
